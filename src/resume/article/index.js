9a57fb4601061d91d63d1a988e7159e33f033e37
Rob Martin <rmartin3@alumni.unca.edu>
2022-04-30 18:08:27 -0400

    styles plus media experiment.

--- src/resume/article/index.js
+++ src/resume/article/index.js
@@ -19,12 +19,12 @@ export default function Article() {
         As we shared in the release post, React 18 introduces features powered
         by our new concurrent renderer, with a gradual adoption strategy for
         existing applications. In this post, we will guide you through the steps
-        for upgrading to React 18. Please report any issues you encounter while
-        upgrading to React 18. Note for React Native users: React 18 will ship
-        in a future version of React Native. This is because React 18 relies on
-        the New React Native Architecture to benefit from the new capabilities
-        presented in this blogpost. For more information, see the React Conf
-        keynote here.
+        for upgrading to React 18. Please report any issues you encounter
+        whilexxx upgrading to React 18. Note for React Native users: React 18
+        will ship in a future version of React Native. This is because React 18
+        relies on the New React Native Architecture to benefit from the new
+        capabilities presented in this blogpost. For more information, see the
+        React Conf keynote here.
       </Typography>
       <Typography variant="h2">Installing</Typography>
       <Typography>To install the latest version of React:</Typography>
